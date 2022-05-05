d34a5c34b860df066eb1060957fa64e285998e93
Santiago <101482317+Santiagovillalba258@users.noreply.github.com>
2022-05-05 10:27:22 -0300

    Add files via upload

---
+++ Mi_Repositorio/Laboratorio-OB-main/Laboratorio-OB-main/c#/EJER04/1/EJER04/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+namespace EJER04
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            // instanciacion de la clase
+
+            microondas objmicroondas = new microondas();
+
+
+            // carga de informacion
+
+            Console.WriteLine("Ingrese el color");
+
+            objmicroondas.color = Console.ReadLine();
+
+
+            Console.WriteLine("Ingrese el marca");
+
+            objmicroondas.marca = Console.ReadLine();
+
+
+            Console.WriteLine("Ingrese la tamaño");
+
+            objmicroondas.tamaño = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Ingrese la potencia");
+
+            objmicroondas.potencia = Convert.ToInt32(Console.ReadLine());
+
+
+            // salida de informacion
+
+            objmicroondas.saludar();
+
+            objmicroondas.mostrarDatos();
+
+        }
+
+
+
+        public class microondas
+        {
+
+
+            // constructor
+
+            public microondas()
+            {
+
+            }
+
+
+            // atributos
+
+            public string color;
+
+            public string marca;
+
+            public int tamaño;
+
+            public int potencia;
+
+
+            // metodos
+
+            public void saludar()
+            {
+
+                Console.WriteLine("Buenos días");
+
+            }
+
+            public void mostrarDatos()
+            {
+
+                Console.WriteLine("El Microondas de color {0} es de la marca {1}  tiene un tamaño de {2}cm y una potencia de {3}hz", color, marca, tamaño, potencia);
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Mi_Repositorio/Laboratorio-OB-main/Laboratorio-OB-main/c#/Solucion1/ejercicio1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace consola01
+{
+internal class Program
+{
+static void Main(string[] args)
+{
+// instanciacion de la clase
+persona objPersona = new persona();
+// carga de informacion
+Console.WriteLine(&quot;Ingrese el nombre&quot;);
+objPersona.nombre = Console.ReadLine();
+Console.WriteLine(&quot;Ingrese el apellido&quot;);
+objPersona.apellido = Console.ReadLine();
+Console.WriteLine(&quot;Ingrese la edad&quot;);
+objPersona.edad = Convert.ToInt32( Console.ReadLine() );
+Console.WriteLine(&quot;Ingrese la altura&quot;);
+objPersona.altura = Convert.ToInt32(Console.ReadLine());
+// salida de informacion
+objPersona.saludar();
+objPersona.mostrarDatos();
+}
+
+public class persona {
+// constructor
+public persona() {
+}
+// atributos
+public string nombre;
+public string apellido;
+public int edad;
+public int altura;
+// metodos
+public void saludar() {
+Console.WriteLine(&quot;Buenos días&quot;);
+}
+public void mostrarDatos() {
+Console.WriteLine(&quot;El sr/a {0} {1} tiene {2} años y mide {3}cm&quot;, nombre, apellido,edad,altura);
+}
+}
+}
+}
